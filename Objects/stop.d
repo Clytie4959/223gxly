51283962545ec2095b1969a072034e210dbaefe3
Clytie4959 <781545141@qq.com>
2018-05-22 21:26:15 +0800

    Signed-off-by: Clytie4959 <781545141@qq.com>

---
+++ Objects/stop.d
@@ -0,0 +1,7 @@
+.\objects\stop.o: stop.c
+.\objects\stop.o: F:\ARM\PACK\Keil\LPC1100_DFP\1.4.0\Device\Include\LPC11xx\LPC11xx.h
+.\objects\stop.o: F:\ARM\PACK\ARM\CMSIS\5.0.0-Beta12\CMSIS\Include\core_cm0.h
+.\objects\stop.o: F:\ARM\ARMCC\Bin\..\include\stdint.h
+.\objects\stop.o: F:\ARM\PACK\ARM\CMSIS\5.0.0-Beta12\CMSIS\Include\cmsis_compiler.h
+.\objects\stop.o: F:\ARM\PACK\ARM\CMSIS\5.0.0-Beta12\CMSIS\Include\cmsis_armcc.h
+.\objects\stop.o: F:\ARM\PACK\Keil\LPC1100_DFP\1.4.0\Device\Include\LPC11xx\system_LPC11xx.h
